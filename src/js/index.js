6d5aa77d84baa0f9c6d6ccde20889ec2cf25beef
kazuya-m <kazuya.matsuo5@gmail.com>
2020-04-23 21:41:37 +0900

    study

--- src/js/index.js
+++ src/js/index.js
@@ -47,18 +47,18 @@
 //-----------------------------apply call bind
 // let arrayyyy = [0,1,2,3,4]
 
-function greet(id) {
-  let slicedArray = [].slice.call(arguments, 0,3);
-  console.log(arguments);
-  console.log(slicedArray);
-  let hi = `hi, ${this.name}`;
-  console.log(hi);
+// function greet(id) {
+//   let slicedArray = [].slice.call(arguments, 0,3);
+//   console.log(arguments);
+//   console.log(slicedArray);
+//   let hi = `hi, ${this.name}`;
+//   console.log(hi);
 
-}
+// }
 
-let obj = {name: "Tom"};
+// let obj = {name: "Tom"};
 
-greet.call(obj, "aaa", "aaa"); //call→呼び出し先のthisを指定する。objを使ってください
+// greet.call(obj, "aaa", "aaa"); //call→呼び出し先のthisを指定する。objを使ってください
 
 // greet.apply(obj [1,2,3]);
 
@@ -163,10 +163,78 @@ greet.call(obj, "aaa", "aaa"); //call→呼び出し先のthisを指定する。
 // console.log(a);
 
 //rest parameters func引数がスプレット
-let sum = (a, ...args) => {
-  console.log(args);
-  return args.reduce((previous, current) => {
-    return previous + current;
-  });
+// let sum = (...args) => {
+//   console.log(args);
+//   return args.reduce((previous, current) => {
+//     return previous + current;
+//   });
+// }
+
+// console.log(sum(1,2,3));
+
+
+//------------------分割代入
+// let a, b, rest;
+// [a, b] = [10, 20]; 
+// console.log(a);
+// console.log(b);
+
+// let c, d, rest1;
+// [c, d, ...rest1] = [10, 20, 30, 40, 50];
+// console.log(c); //10
+// console.log(d); //20
+// console.log(rest1); //[30 ~ 50]
+
+// let e, f, rest2;
+// [e] = [10, 20, 30, 40, 50];
+// console.log(e); //10
+// [e,,f] = [10, 20, 30, 40, 50];
+// console.log(e); //10
+// console.log(f); //30
+
+// let g, h, i;
+// [g,,h,i=1] = [10, 20, 30];
+// console.log(g); //10
+// console.log(h); //30
+// console.log(i); //1
+
+// //obg分割代入 要プロパティ一致
+// const { p1: p11, p2, ...restp} = {p1: 10, p2: 20, p3: 30, p4: 40};
+// console.log(p11); //10
+// console.log(p2); //20
+// console.log(restp); //{p3 ~ p4}
+
+//関数の引数を分割代入
+//引数がある場合はそれが入り、ない場合は初期値。右辺の空オブジェクトがないと引数なしはエラー。
+//左辺に初期値は代入する右辺に値なしの場合にセットされるから。
+// const drawES2015Chart = ({size = 'big', coords = {x: 0, y: 0}, radius = 25} = {}) => { 
+//   console.log(size);  //渡されたさいず
+//   console.log(coords); //渡されたcoords obj
+//   console.log(radius); //25
+// }
+
+// drawES2015Chart({
+//   size: 'hahaha',
+//   coords: { c: 111, y: 222},
+// });
+
+// -----------------テンプレートリテラル(バックスラッシュ)
+//変数追加とかで使う
+//old
+// let str = "hello";
+// let str1 = "Hello" + str;
+// //new
+// let str2 = `Hello\n${str}`;
+// console.log(str1);
+// console.log(str2);
+
+const bFunc = (strings, ...values) => {
+  return strings.reduce((accu, str, i) => {
+    let val = values[i] ? `${values[i]}` : "";
+    return `${accu}${str}${val}`
+  }, '');
 }
-console.log(sum(1,2,3));
+const str1 = "bob";
+const str2 = "david";
+const result = bFunc`${str1} and ${str2}12345678`;
+console.log(result);
